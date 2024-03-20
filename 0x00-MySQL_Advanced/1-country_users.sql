259fbd64f395deb3a82c4acc1ecd4b1904d5d3cb
thedunncodes <tobioluwa348@gmail.com>
2024-03-20 12:01:47 +0000

    Country users

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Creates a table 'users'
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	name VARCHAR(255),
+	country VARCHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+);
