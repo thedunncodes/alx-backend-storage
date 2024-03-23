42f035674495156843e310b17b56494bf3d123b5
thedunncodes <tobioluwa348@gmail.com>
2024-03-23 16:02:59 +0000

    glam

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- lists bands with 'Glam rock' as main style
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-	FROM metal_bands
-	WHERE style LIKE '%Glam rock%'
-	ORDER BY lifespan DESC;
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
