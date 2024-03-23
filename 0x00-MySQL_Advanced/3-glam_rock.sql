fab9481919795c6b3e7d4dee69a68211acbd4ff6
thedunncodes <tobioluwa348@gmail.com>
2024-03-23 15:50:25 +0000

    glam rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- lists bands with 'Glam rock' as main style
-SELECT band_name, (IFNULL(split, '2020') - formed) AS longevity
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
 	FROM metal_bands
 	WHERE style LIKE '%Glam rock%'
 	ORDER BY longevity DESC, id DESC;
